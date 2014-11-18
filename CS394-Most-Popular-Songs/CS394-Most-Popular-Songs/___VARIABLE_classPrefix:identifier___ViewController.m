1068243a297c8bc9a52f00ba7ff2d5757e9d02b8
Joel Castillo <joel.castillo@nyu.edu>
2014-11-18 18:19:45 -0500

    Initial Commit - Empty Firebase Project

---
+++ CS394-Most-Popular-Songs/CS394-Most-Popular-Songs/___VARIABLE_classPrefix:identifier___ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ___VARIABLE_classPrefix:identifier___ViewController.m
+//  CS394-Most-Popular-Songs
+//
+//  Created by Joel Castillo on 11/18/14.
+//  Copyright (c) 2014 Joel Castillo. All rights reserved.
+//
+
+@interface ___VARIABLE_classPrefix:identifier___ViewController ()
+
+@end
+
+class implementation {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
