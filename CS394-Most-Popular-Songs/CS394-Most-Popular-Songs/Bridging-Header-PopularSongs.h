199e214898bc8e49383f38c6a7ed943fc5bb955d
usrfrndly <jmh794@nyu.edu>
2014-11-18 18:52:28 -0500

    bridging_header

---
+++ CS394-Most-Popular-Songs/CS394-Most-Popular-Songs/Bridging-Header-PopularSongs.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header-PopularSongs.h
+//  CS394-Most-Popular-Songs
+//
+//  Created by Jaclyn May on 11/18/14.
+//  Copyright (c) 2014 Joel Castillo. All rights reserved.
+//
+
+#ifndef CS394_Most_Popular_Songs_Bridging_Header_PopularSongs_h
+#define CS394_Most_Popular_Songs_Bridging_Header_PopularSongs_h
+
+#import <Firebase/Firebase.h>
+
+#endif
