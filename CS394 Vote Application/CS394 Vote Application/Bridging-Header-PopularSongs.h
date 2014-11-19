b5f59553e6e5bc4e94328614f7b3af9c98ac8bdb
usrfrndly <jmh794@nyu.edu>
2014-11-18 19:07:55 -0500

    FIREBASE!!

---
+++ CS394 Vote Application/CS394 Vote Application/Bridging-Header-PopularSongs.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header-PopularSongs.h
+//  CS394 Vote Application
+//
+//  Created by Jaclyn May on 11/18/14.
+//  Copyright (c) 2014 Philipp Marsch. All rights reserved.
+//
+
+#ifndef CS394_Vote_Application_Bridging_Header_PopularSongs_h
+#define CS394_Vote_Application_Bridging_Header_PopularSongs_h
+
+
+#endif
---
+++ PopularSongsCompare/PopularSongsCompare/Bridging-Header-PopularSongsCompare.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header-PopularSongsCompare.h
+//  PopularSongsCompare
+//
+//  Created by Jaclyn May on 11/18/14.
+//  Copyright (c) 2014 NYU. All rights reserved.
+//
+
+#ifndef PopularSongsCompare_Bridging_Header_PopularSongsCompare_h
+#define PopularSongsCompare_Bridging_Header_PopularSongsCompare_h
+
+
+#endif
