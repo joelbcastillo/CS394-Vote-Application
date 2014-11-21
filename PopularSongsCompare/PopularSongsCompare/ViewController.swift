25d9c99d4757c0f1e909ae02760622e3a4444ae9
Philipp Marsch <pmarsch@stud.hs-bremen.de>
2014-11-21 14:22:11 -0500

    fixed compile errors

--- PopularSongsCompare/PopularSongsCompare/ViewController.swift
+++ PopularSongsCompare/PopularSongsCompare/ViewController.swift
@@ -13,14 +13,20 @@ class ViewController: UIViewController {
     let myRootRef = Firebase(url:"https://popularsongscompared.firebaseio.com/")
     
     @IBOutlet weak var title1: UILabel!
-    
     @IBOutlet weak var title2: UILabel!
-
     @IBOutlet weak var image1: UIImageView!
-
     @IBOutlet weak var image2: UIImageView!
+    @IBOutlet weak var songOneLabel: UILabel!
+    @IBOutlet weak var songTwoLabel: UILabel!
+    @IBOutlet weak var songOneImage: UIImageView!
+    @IBOutlet weak var songTwoImage: UIImageView!
+    
     var id1:Int?
     var id2:Int?
+    
+    var imageOneScore = 0
+    var imageTwoScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -30,16 +36,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    @IBOutlet weak var songOneLabel: UILabel!
-
-    @IBOutlet weak var songTwoLabel: UILabel!
-    @IBOutlet weak var songOneImage: UIImageView!
-    
-    @IBOutlet weak var songTwoImage: UIImageView!
-    
-    var imageOneScore = 0
-    var imageTwoScore = 0
     
     func get_two_songs(){
         var randomIndex1 = randomIndex() //to add
@@ -54,10 +50,11 @@ class ViewController: UIViewController {
         var imageURL2 = songURL2.childByAppendingPath("link/1")
 
         songURL1.observeEventType(FEventType.Value, withBlock: { (snapshot) in
-            self.title1  = snapshot.value["title"] as? String
+            var title  = snapshot.value["title"] as? String
+            self.title1.text = title
         })
         
-        songOneLabel.text = self.title1;
+        songOneLabel.text = self.title1.text;
 
         
         imageURL1.observeEventType(FEventType.Value, withBlock: { (snapshot) in
@@ -66,18 +63,15 @@ class ViewController: UIViewController {
             self.image1.image = imagez
 
         })
-        var url = NSURL(fileURLWithPath: self.image1)
-        var error = NSError?()
-        var imageData = NSData(contentsOfURL: url!)
         
-        songOneImage = UIImageView(image: UIImage(data: imageData!)  )
+        songOneImage = self.image1
         
 
         songURL2.observeEventType(FEventType.Value, withBlock: { (snapshot) in
-            self.title2  = snapshot.value["title"] as? String
+            self.title2.text  = snapshot.value["title"] as? String
         })
         
-        songTwoLabel.text = self.title2;
+        songTwoLabel.text = self.title2.text;
 
         
         imageURL2.observeEventType(FEventType.Value, withBlock: { (snapshot) in
@@ -87,39 +81,39 @@ class ViewController: UIViewController {
             
         })
         
+        songTwoImage = self.image2
     }
     
     @IBAction func selectSong1(sender: UIButton) {
         
         //UPDATE
         var votesURL = myRootRef.childByAppendingPath("feed/entry/\(self.id1)")
-        var votes:Int
+        // TODO: Votes gets instantiated every time this method will be accessed
+        var votes:Int = 0;
 
         votesURL.observeEventType(FEventType.Value, withBlock: { (snapshot) in
-            votes = snapshot.value["votes"] as? Int?
+            votes = snapshot.value["votes"] as Int
         
         })
         votes = votes + 1
-        votesUrl.setValue(votes)
-    
+        votesURL.setValue(votes)
     }
     
     @IBAction func selectSong2(sender: UIButton) {
         
         //UPDATE
         var votesURL = myRootRef.childByAppendingPath("feed/entry/\(self.id2)")
-        var votes:Int
+        var votes:Int = 0
         
         votesURL.observeEventType(FEventType.Value, withBlock: { (snapshot) in
-            votes = snapshot.value["votes"] as? Int?
+            votes = snapshot.value["votes"] as Int
             
         })
-        votes = votes + 1
-        votesUrl.setValue(votes)
         
-        url = NSURL(fileURLWithPath: self.image2)
-        imageData = NSData(contentsOfURL: url!)
-        songTwoImage = UIImageView(image: UIImage(data: imageData!)  )
+        votes = votes + 1
+        votesURL.setValue(votes)
+        
+        songTwoImage = self.image2
 
         
     }
@@ -133,6 +127,10 @@ class ViewController: UIViewController {
                     
                 }
         }
+    }
+    
+    func randomIndex() -> Int {
+        return Int(arc4random_uniform(99))
     }
 
 }
